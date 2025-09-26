5bbadd0f5486d829bd4f8b410e52d742d194aa7a
Renna Carver <87547832+rennacarver@users.noreply.github.com>
2025-09-26 11:51:16 -0700

    add sytle and css animations

--- src/Main.elm
+++ src/Main.elm
@@ -10,6 +10,7 @@ module Main exposing (..)
 import Browser
 import Html exposing (Html, button, div, text)
 import Html.Events exposing (onClick)
+import Html.Attributes exposing (style)
 
 
 
@@ -21,15 +22,19 @@ main =
 
 
 
--- MODEL
 
 
-type alias Model = Int
+type alias Model =
+  { count : Int
+  , rotation : Int
+  }
 
 
 init : Model
 init =
-  0
+  { count = 0
+  , rotation = 0
+  }
 
 
 
@@ -45,10 +50,13 @@ update : Msg -> Model -> Model
 update msg model =
   case msg of
     Increment ->
-      model + 1
+      { model
+        | count = model.count + 1
+        , rotation = model.rotation + 90
+      }
 
     Decrement ->
-      model - 1
+      { model | count = model.count - 1 }
 
 
 
@@ -59,6 +67,14 @@ view : Model -> Html Msg
 view model =
   div []
     [ button [ onClick Decrement ] [ text "-" ]
-    , div [] [ text (String.fromInt model) ]
-    , button [ onClick Increment ] [ text "+" ]
+    , div
+        [ style "display" "inline-block"
+        , style "minWidth" "2ch"
+        , style "textAlign" "center"
+        , style "transformOrigin" "50% 50%"
+        , style "transition" "transform 200ms ease"
+        , style "transform" ("rotate(" ++ String.fromInt model.rotation ++ "deg)")
+        ]
+        [ text (String.fromInt model.count) ]
+    , button [ onClick Increment, style "backgroundColor" "blue", style "color" "white" ] [ text "+" ]
     ]
